f1f3992c0c3e0217156d1f61b2922e64703c76c8
unknown <varadesharad@gmail.com>
2020-04-21 01:44:11 +0530

    ADD[usecase 6 and 7]

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -2,17 +2,23 @@
 
 #CONSTANT
 PLAYER_1=0
+PLAYER_2=1
 No_Play=0
 Ladder=1
 Snake=2
+WINNING_POSITION=100
 
 #VARIABLE
+diceCount=0
 PlayerPosition=0
 playerOnePosition=0
+playerTwoPosition=0
+turn=0
 
 #player rolling Die
 function rollingDie() {
 	result=$((RANDOM%6+1))
+	((diceCount++))
 	echo $result
 }
 
@@ -34,12 +40,66 @@ function checkOption(){
 					fi
 					;;
 		esac
+		getPreviousPosition
 }
 
+function getPreviousPosition(){
+	if [ $PlayerPosition -gt $WINNING_POSITION ];
+	then
+			PlayerPosition=$(( $PlayerPosition - $result ))
+	fi
+}
+
+function playerTwo(){
+	PlayerPosition=$playerTwoPosition
+	rillingDie
+	checkOption
+	playerTwoPosition=$PlayerPosition
+	turn=0
+	getWinningPlayer
+}
 
 function playerOne(){
 	PlayerPosition=$playerOnePosition
 	rollingDie
 	checkOption
 	playerOnePosition=$PlayerPosition
+	turn=1
+	getWinningPlayer
 }
+
+function turnPlayer() {
+	if [ $turn -eq 0 ]
+	then
+			playerOne
+	else
+			playerTwo
+	fi
+}
+
+function getWinningPlayer () {
+	if [ $playerOnePosition -eq $WINNING_POSITION ];
+	then
+			echo "player One win..."
+	elif [ $playerTwoPosition -eq $WINNING_POSITION ];
+	then
+			echo "player Two Win..."
+	fi
+}
+
+function startGame(){
+	while [ $PlayerPosition != $WINNING_POSITION ]
+	do
+			turnPlayer
+	done
+}
+
+#rollinDie
+#echo $result
+#start playing first player
+echo "Player Position: " $PlayerPosition
+checkOption
+echo "Player Position: " $PlayerPosition
+#Two Player Game
+startGame
+echo "Total Dice Roll: " $diceCount
