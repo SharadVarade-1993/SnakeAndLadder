4d6aff7f4b54cca25b38135312ed509934c6d9ca
unknown <varadesharad@gmail.com>
2020-04-20 20:05:25 +0530

    Add[usecase3].To add player play the game or not or reverse

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -1,6 +1,31 @@
 #!/bin/bash -x
 
+#CONSTANT
 PLAYER_1=0
+No_Play=0
+Ladder=1
+Snake=2
+
+#VARIABLE
+PlayerPosition=0
 
 result=$((RANDOM%6+1))
 echo $result
+
+option=$((RANDOM%3))
+case $option in
+	$No_Play)
+				PlayerPosition=$PlayerPosition
+				;;
+	$Ladder)
+				PlayerPosition=$(( $PlayerPosition + $result ))
+				;;
+	$Snake)
+				PlayerPosition=$(( $PlayerPosition - $result ))
+				if [ $PlayerPosition -lt 0]
+				then
+						PlayerPosition=0
+				fi
+				;;
+	esac
+
