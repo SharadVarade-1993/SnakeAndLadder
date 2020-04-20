c8c0e29137382658d335ae818b201f1cc9567aef
unknown <varadesharad@gmail.com>
2020-04-20 21:24:31 +0530

    Add[usecase4]

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -8,24 +8,38 @@ Snake=2
 
 #VARIABLE
 PlayerPosition=0
+playerOnePosition=0
 
-result=$((RANDOM%6+1))
-echo $result
+#player rolling Die
+function rollingDie() {
+	result=$((RANDOM%6+1))
+	echo $result
+}
 
-option=$((RANDOM%3))
-case $option in
-	$No_Play)
-				PlayerPosition=$PlayerPosition
-				;;
-	$Ladder)
-				PlayerPosition=$(( $PlayerPosition + $result ))
-				;;
-	$Snake)
-				PlayerPosition=$(( $PlayerPosition - $result ))
-				if [ $PlayerPosition -lt 0]
-				then
-						PlayerPosition=0
-				fi
-				;;
-	esac
+#check player option
+function checkOption(){
+	option=$((RANDOM%3))
+	case $option in
+		$No_Play)
+					PlayerPosition=$PlayerPosition
+					;;
+		$Ladder)
+					PlayerPosition=$(( $PlayerPosition + $result ))
+					;;
+		$Snake)
+					PlayerPosition=$(( $PlayerPosition - $result ))
+					if [ $PlayerPosition -lt 0]
+					then
+							PlayerPosition=0
+					fi
+					;;
+		esac
+}
 
+
+function playerOne(){
+	PlayerPosition=$playerOnePosition
+	rollingDie
+	checkOption
+	playerOnePosition=$PlayerPosition
+}
